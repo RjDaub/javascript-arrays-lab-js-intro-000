e3e824439d55389442a68fbbd32674d036874459
Robert Daub <rjd2066@gmail.com>
2020-04-07 00:37:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 
 function destructivelyAppendKitten(kittens) {
- kittens  ["Milo", "Otis", "Garfield"]
+ var kittens = ["Milo", "Otis", "Garfield"]
   kittens.push("Ralph")
 
 }
