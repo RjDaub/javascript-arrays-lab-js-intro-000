0ad3f5538e486aff632f142a8d6dfe896c9179c4
Robert Daub <rjd2066@gmail.com>
2020-04-07 00:55:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function destructivelyAppendKitten(Milo, Otis, Garfield) {
   kittens.push('Ralph')
 }
 kittens();
+
+function destructivelyAppendKitten(Milo, Otis, Garfield) {
+  kittens.unshift('Ralph')
+}
+kittens();
